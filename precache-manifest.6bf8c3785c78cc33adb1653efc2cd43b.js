495d8d8d523297323331f1c6c1ab8a922b983c12
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-13 15:12:07 +0000

    Deploy luigimannoni/webxr-earthday to github.com/luigimannoni/webxr-earthday.git:gh-pages

---
+++ precache-manifest.6bf8c3785c78cc33adb1653efc2cd43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef2ef29cb0bf9b0dd13004a2db6bc81",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d698ea689dad7c7b36fe",
+    "url": "./static/css/main.def0854b.chunk.css"
+  },
+  {
+    "revision": "d0197c4acaa1a8f35784",
+    "url": "./static/js/2.d79341cc.chunk.js"
+  },
+  {
+    "revision": "a2a63543ccf2528e17d717a3d7afbb1a",
+    "url": "./static/js/2.d79341cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d698ea689dad7c7b36fe",
+    "url": "./static/js/main.c77b0430.chunk.js"
+  },
+  {
+    "revision": "3924f1822dd59d4852ad",
+    "url": "./static/js/runtime-main.4b4eb50d.js"
+  }
+]);
